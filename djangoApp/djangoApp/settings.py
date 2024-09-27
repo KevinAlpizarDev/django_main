40202afbb8563410397a49df2b5fdf9887b24634
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-09-27 16:39:40 -0600

    Recreando

--- djangoApp/djangoApp/settings.py
+++ djangoApp/djangoApp/settings.py
@@ -90,17 +90,17 @@ WSGI_APPLICATION = "djangoApp.wsgi.application"
 #     }
 # }
 
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "example",
         "USER": "root",
         "PASSWORD": "kevinalpizar1998@PUTOamo",
-        "HOST": "localhost",
+        "HOST": "mysql",
         "PORT": "3306",
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
